d4f104ed290f79ae17f091b5f04ffacb6314d5d9
Steve Tyler <steve.tyler75@gmail.com>
2024-05-10 18:32:37 +0100

    section 171

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -3,6 +3,7 @@ import { createContext } from 'react';
 export const CartContext = createContext({
     // use for autocompletion
     items: [],
-    addItemToCart: () => {}
+    addItemToCart: () => {},
+    updateItemQuantity: () => {}
 });
 
